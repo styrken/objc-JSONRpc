2765091047970a2cc9cb6ce01ed0ad930039ed00
Rasmus Styrk <rts@Rasmuss-iMac.local>
2012-08-29 11:43:51 +0200

    Renmaed rpc classes (and files). Moved invoking of rpc methods into a category instead

---
+++ objc-JSONRpc/RPC Client/Helpers/Invoke/BaseRPCClient+InvokeMethods.h
@@ -0,0 +1,13 @@
+//
+//  BaseRPCClient+InvokeMethods.h
+//  objc-JSONRpc
+//
+//  Created by Rasmus Styrk on 29/08/12.
+//  Copyright (c) 2012 Rasmus Styrk. All rights reserved.
+//
+
+#import "BaseRPCClient.h"
+
+@interface BaseRPCClient (InvokeMethods)
+
+@end
---
+++ objc-JSONRpc/RPC Client/Helpers/Invoke/BaseRPCClient+InvokeMethods.m
@@ -0,0 +1,13 @@
+//
+//  BaseRPCClient+InvokeMethods.m
+//  objc-JSONRpc
+//
+//  Created by Rasmus Styrk on 29/08/12.
+//  Copyright (c) 2012 Rasmus Styrk. All rights reserved.
+//
+
+#import "BaseRPCClient+InvokeMethods.h"
+
+@implementation BaseRPCClient (InvokeMethods)
+
+@end
