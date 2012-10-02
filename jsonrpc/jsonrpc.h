fd0de6dafcab8bd1f45e9987b419a03b4c459055
Rasmus Styrk <rts@Rasmuss-iMac.local>
2012-10-02 09:31:20 +0200

    Added new target for creating static library

---
+++ jsonrpc/jsonrpc.h
@@ -0,0 +1,13 @@
+//
+//  jsonrpc.h
+//  jsonrpc
+//
+//  Created by Rasmus Styrk on 02/10/12.
+//  Copyright (c) 2012 Rasmus Styrk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsonrpc : NSObject
+
+@end
---
+++ jsonrpc/jsonrpc.m
@@ -0,0 +1,13 @@
+//
+//  jsonrpc.m
+//  jsonrpc
+//
+//  Created by Rasmus Styrk on 02/10/12.
+//  Copyright (c) 2012 Rasmus Styrk. All rights reserved.
+//
+
+#import "jsonrpc.h"
+
+@implementation jsonrpc
+
+@end
